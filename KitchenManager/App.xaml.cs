634975b0be02fc83d088ee744030addbb8daa371
James Makela <jamesilmarimakela@outlook.com>
2024-03-12 15:15:57 +0800

    Add project files.

---
+++ KitchenManager/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KitchenManager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
