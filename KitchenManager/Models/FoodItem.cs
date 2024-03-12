3aea674e885e50d47970c68df00bde1f114a0577
James Makela <jamesilmarimakela@outlook.com>
2024-03-12 15:50:44 +0800

    Start models.

---
+++ KitchenManager/Models/FoodItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitchenManager.Models
+{
+    internal class FoodItem
+    {
+        public string ID {  get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        // TODO: Do I want to create an enum for unit possibilities?
+        public string PriceUnit { get; set; }
+
+        // TODO: How will this initialise - will this start at 0?
+        public int CurrentStock { get; set; }
+
+        public FoodItem(string iD, string name, decimal price, string priceUnit, int currentStock)
+        {
+            ID = iD;
+            Name = name;
+            Price = price;
+            PriceUnit = priceUnit;
+            CurrentStock = currentStock;
+        }
+    }
+
+    
+}
