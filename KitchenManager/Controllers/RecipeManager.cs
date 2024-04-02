fec288f5b256ff26a5584a3e87fb3cff209ccdf9
James Makela <jamesilmarimakela@outlook.com>
2024-04-03 07:53:48 +0800

    feat: Recipe Card 2.0.

---
+++ KitchenManager/Controllers/RecipeManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KitchenManager.Models;
+
+namespace KitchenManager.Controllers
+{
+    class RecipeManager
+    {
+        public Recipe CurrentRecipe { get; set; }
+
+        public RecipeManager() { }
+
+        public void ChangeYield(int newYield)
+        {
+            int oldYield = Convert.ToInt32(CurrentRecipe.Yield);
+            CurrentRecipe.Yield = newYield;
+            List<FoodItem> ingredients = new();
+            foreach (FoodItem ingredient in CurrentRecipe.Ingredients)
+            {
+                decimal oldQuantity = ingredient.Quantity;
+                ingredient.Quantity = (oldQuantity / oldYield) * newYield;
+            }
+
+        }
+    }
+}
--- KitchenManager/RecipeCardPage.xaml.cs
+++ KitchenManager/RecipeCardPage.xaml.cs
@@ -1,23 +1,43 @@
 using KitchenManager.Models;
+using KitchenManager.Controllers;
 
 namespace KitchenManager;
 
 public partial class RecipeCardPage : ContentPage
 {
+	RecipeManager manager = new();
 	public RecipeCardPage(Recipe recipe)
 	{
+		manager.CurrentRecipe = recipe;
 		InitializeComponent();
-		PopulateFields(recipe);
+		PopulateFields(manager.CurrentRecipe);
 	}
 
     void PopulateFields(Recipe recipe)
     {
+		int yield = Convert.ToInt32(recipe.Yield);
+
 		Label_RecipeLabel.Text = recipe.Label;
 		Label_RecipeSourceName.Text = recipe.SourceName;
-		Label_RecipeSourceURL.Text = recipe.SourceLink;
-		Label_RecipeYield.Text = $"Yield: {recipe.Yield}";
+		Label_RecipeYield.Text = $"Yield: {yield}";
+		ListView_Ingredients.ItemsSource = null;
 		ListView_Ingredients.ItemsSource = recipe.Ingredients;
 		Image_RecipeImage.Source = recipe.ImageLink;
-		Label_RecipeID.Text = recipe.URI;
+    }
+
+    private void Button_DecreaseYield_Pressed(object sender, EventArgs e)
+    {
+		int.TryParse(Label_RecipeYield.Text.Split(":")[1], out int yield);
+		yield--;
+		manager.ChangeYield(yield);
+		PopulateFields(manager.CurrentRecipe);
+    }
+
+    private void Button_IncreaseYield_Pressed(object sender, EventArgs e)
+    {
+        int.TryParse(Label_RecipeYield.Text.Split(":")[1], out int yield);
+        yield++;
+        manager.ChangeYield(yield);
+        PopulateFields(manager.CurrentRecipe);
     }
 }
