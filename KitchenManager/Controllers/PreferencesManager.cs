3e9c895005a536db844ba266137bb52a44be74c7
James Makela <jamesilmarimakela@outlook.com>
2024-05-03 16:44:16 +0800

    First preference created, not yet implemented.

---
+++ KitchenManager/Controllers/PreferencesManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitchenManager.Controllers
+{
+    internal static class PreferencesManager
+    {
+
+        public static bool GetUnit()
+        {
+            return Preferences.Default.Get<bool>("units", true);
+        }
+
+        public static void SetUnit(bool value)
+        {
+            Preferences.Default.Set<bool>("units", value);
+        }
+    }
+}
--- KitchenManager/PreferencesPage.xaml.cs
+++ KitchenManager/PreferencesPage.xaml.cs
@@ -1,3 +1,5 @@
+using KitchenManager.Controllers;
+
 namespace KitchenManager;
 
 public partial class PreferencesPage : ContentPage
@@ -5,5 +7,26 @@ public partial class PreferencesPage : ContentPage
 	public PreferencesPage()
 	{
 		InitializeComponent();
+		PopulateFields();
+	}
+
+    private void Picker_Unit_SelectedIndexChanged(object sender, EventArgs e)
+    {
+		string? value = Picker_Unit.SelectedItem as string;
+		if (value == "Metric")
+		{
+			PreferencesManager.SetUnit(true);
+		}
+		if (value == "Imperial")
+		{
+			PreferencesManager.SetUnit(false);
+		}
+		
+    }
+
+	private void PopulateFields()
+	{
+		bool unit = PreferencesManager.GetUnit();
+		Picker_Unit.SelectedIndex = unit ? 0 : 1;
 	}
 }
